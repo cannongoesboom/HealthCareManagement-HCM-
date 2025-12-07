0ce95f1cf2c426f91961359bc36df3b1de91aa46
CannonGoesBoom <will@cannongoesboom.com>
2025-12-07 18:44:16 +0800

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,1 +1,13 @@
+<?php
+ $servername = "localhost";
+ $username = "root";
+ $password = "";
 
+try {
+  $db_con = new PDO("mysql:host=$servername;dbname=healthcare", $username, $password);
+  // set the PDO error mode to exception
+  $db_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+?>
